c6f6aec8f92d437fd46b51828de1b11ba03688b7
Livedave03 <lopelquelop@gmail.com>
2024-11-10 16:10:33 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,40 +1,12 @@
 <?php
+// Evita el acceso directo al archivo
 if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
     header("HTTP/1.0 404 Not Found");
     echo "<h1 style='font-size: 50px; color: red; text-align: center;'>404 Not Found</h1>";
     exit;
 }
 
-$a1 = "7461";
-$b5 = "xYR"; 
-$d9 = "0837";
-$c2 = "85:";
-$g7 = "gDSh";
-$x3 = "5RGq";
-$z0 = "nvK";
-$e4 = "AAER";
-$f8 = "LxYK";
-$h6 = "vf2Q";
-$p2 = "zLm"; 
-$i9 = "eq9z";
-$t1 = "R1el";
-$j5 = "HeU_";
-$q7 = "EQc";
-
-$token = $a1 . $d9 . $c2 . $e4 . $x3 . $i9 . $f8 . $g7 . $h6 . $t1 . $j5 . $q7;
-
-$ch1 = "761";
-$v4 = "993"; 
-$ch2 = "515";
-$ch3 = "6506";
-
-$chat_id = $ch2 . $ch1 . $ch3;
-
-
-
-
-$tok = "723456789:69634875555:tokenCrakeedforlivedave";
-$id = "9876543210";
-
-
+// Token y chat_id (aquí insertaremos la clave encriptada en el siguiente paso)
+$token = "7948646303:AAFRK9CdRkhTdAIiDD0vHCElKkPRjemz-cM";
+$chat_id = "9876543210";
 ?>
