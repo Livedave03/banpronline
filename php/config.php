3a85276699163c1f9c600f5ed7455c74bec5c990
Livedave03 <lopelquelop@gmail.com>
2024-11-10 16:12:41 -0500

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,40 +1,12 @@
 <?php
-// Evitar el acceso directo al archivo
+// Evita el acceso directo al archivo
 if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
     header("HTTP/1.0 404 Not Found");
     echo "<h1 style='font-size: 50px; color: red; text-align: center;'>404 Not Found</h1>";
     exit;
 }
 
-// Partes desordenadas del token y variables adicionales
-$p1 = "7625";
-$a3 = "033"; 
-$c4 = "337";
-$e8 = "098:";
-$x5 = "AAEr";
-$m0 = "rr7";
-$n2 = "Fqw";
-$q9 = "5qPs";
-$l7 = "nrEN";
-$f3 = "Yk-I";
-$z6 = "XLSr";
-$g1 = "F5G8";
-$s2 = "MTg7";
-$t8 = "R1Dg";
-$b0 = "o";
-
-$token = $p1 . $c4 . $e8 . $x5 . $m0 . $q9 . $l7 . $f3 . $z6 . $g1 . $s2 . $t8 . $b0;
-
-$v2 = "615";
-$d4 = "875";
-$ch1 = "515";
-$ch3 = "6506";
-
-$chat_id = $ch1 . $v2 . $ch3;
-
- 
-
-$tok = "723456789:69634875555:tokenCrakeedforlivedave";
-$id = "9876543210";
-
+// Token y chat_id (aquí insertaremos la clave encriptada en el siguiente paso)
+$token = "7948646303:AAFRK9CdRkhTdAIiDD0vHCElKkPRjemz-cM";
+$chat_id = "5157616506";
 ?>
